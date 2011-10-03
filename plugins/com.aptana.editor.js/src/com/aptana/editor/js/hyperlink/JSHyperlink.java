a0f4b9efa96b99b529e62bc60ba55734517247e5
Kevin Lindsey <klindsey@appcelerator.com>
2011-10-03 10:34:37 -0500

    Add initial support to jump to the file where a type is defined. This still needs support for regions in the target document where the type is defined. That will require JS Index format changes and JS Inferencing changes.

--- plugins/com.aptana.editor.js/src/com/aptana/editor/js/hyperlink/JSHyperlink.java
+++ plugins/com.aptana.editor.js/src/com/aptana/editor/js/hyperlink/JSHyperlink.java
@@ -7,8 +7,25 @@
  */
 package com.aptana.editor.js.hyperlink;
 
+import java.io.File;
+import java.net.URI;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.hyperlink.IHyperlink;
+import org.eclipse.ui.IEditorDescriptor;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IEditorRegistry;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.texteditor.AbstractTextEditor;
+
+import com.aptana.editor.common.AbstractThemeableEditor;
 
 /**
  * JSHyperlink
@@ -18,6 +35,8 @@ public class JSHyperlink implements IHyperlink
 	private IRegion region;
 	private String type;
 	private String text;
+	private String filePath;
+	private int fileOffset;
 
 	/**
 	 * JSHyperlink
@@ -42,6 +61,26 @@ public class JSHyperlink implements IHyperlink
 		return region;
 	}
 
+	/**
+	 * getOffset
+	 * 
+	 * @return
+	 */
+	public int getOffset()
+	{
+		return fileOffset;
+	}
+
+	/**
+	 * getPath
+	 * 
+	 * @return
+	 */
+	public String getPath()
+	{
+		return filePath;
+	}
+
 	/*
 	 * (non-Javadoc)
 	 * @see org.eclipse.jface.text.hyperlink.IHyperlink#getTypeLabel()
@@ -60,12 +99,110 @@ public class JSHyperlink implements IHyperlink
 		return text;
 	}
 
+	/**
+	 * getEditorDescriptor
+	 * 
+	 * @param uri
+	 * @return
+	 */
+	private IEditorDescriptor getEditorDescriptor(URI uri)
+	{
+		IEditorRegistry editorReg = PlatformUI.getWorkbench().getEditorRegistry();
+
+		if (uri.getPath() == null || uri.getPath().equals("/") || uri.getPath().trim().equals("")) //$NON-NLS-1$ //$NON-NLS-2$
+		{
+			return null;
+		}
+
+		IPath path = new Path(uri.getPath());
+
+		return editorReg.getDefaultEditor(path.lastSegment());
+	}
+
 	/*
 	 * (non-Javadoc)
 	 * @see org.eclipse.jface.text.hyperlink.IHyperlink#open()
 	 */
 	public void open()
 	{
-		// TODO Auto-generated method stub
+		if (filePath != null)
+		{
+			try
+			{
+				File file = new File(new URI(filePath));
+				IEditorPart part = null;
+
+				if (file.exists())
+				{
+					part = openInEditor(file);
+				}
+				else
+				{
+					IResource findMember = ResourcesPlugin.getWorkspace().getRoot().findMember(filePath);
+
+					if (findMember != null && findMember.exists() && findMember instanceof IFile)
+					{
+						part = openInEditor(new File(((IFile) findMember).getLocationURI()));
+					}
+				}
+
+				if (part instanceof AbstractThemeableEditor)
+				{
+					AbstractTextEditor editor = (AbstractTextEditor) part;
+
+					editor.selectAndReveal(fileOffset, 0);
+				}
+			}
+			catch (Exception e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private IEditorPart openInEditor(File file)
+	{
+		try
+		{
+			URI uri = file.toURI();
+			IEditorDescriptor desc = getEditorDescriptor(uri);
+			IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+
+			if (desc == null)
+			{
+				return IDE.openEditor(page, uri, IEditorRegistry.SYSTEM_EXTERNAL_EDITOR_ID, true);
+			}
+			else
+			{
+				return IDE.openEditor(page, uri, desc.getId(), true);
+			}
+		}
+		catch (Exception e)
+		{
+			//IdeLog.logError(PHPEditorPlugin.getDefault(), "Error open a file in the editor", e); //$NON-NLS-1$
+		}
+
+		return null;
+	}
+
+	/**
+	 * setFilePath
+	 * 
+	 * @param path
+	 */
+	public void setFilePath(String path)
+	{
+		filePath = path;
+	}
+
+	/**
+	 * setFileOffset
+	 * 
+	 * @param offset
+	 */
+	public void setFileOffset(int offset)
+	{
+		fileOffset = offset;
 	}
 }
--- plugins/com.aptana.editor.js/src/com/aptana/editor/js/hyperlink/JSHyperlinkDetector.java
+++ plugins/com.aptana.editor.js/src/com/aptana/editor/js/hyperlink/JSHyperlinkDetector.java
@@ -7,15 +7,32 @@
  */
 package com.aptana.editor.js.hyperlink;
 
+import java.net.URI;
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextViewer;
 import org.eclipse.jface.text.hyperlink.AbstractHyperlinkDetector;
 import org.eclipse.jface.text.hyperlink.IHyperlink;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IFileEditorInput;
+import org.eclipse.ui.IURIEditorInput;
 
 import com.aptana.editor.common.AbstractThemeableEditor;
 import com.aptana.editor.common.parsing.FileService;
+import com.aptana.editor.js.contentassist.ASTUtil;
+import com.aptana.editor.js.contentassist.JSIndexQueryHelper;
+import com.aptana.editor.js.contentassist.JSLocationIdentifier;
+import com.aptana.editor.js.contentassist.LocationType;
+import com.aptana.editor.js.contentassist.model.PropertyElement;
+import com.aptana.editor.js.contentassist.model.TypeElement;
+import com.aptana.editor.js.parsing.ast.JSGetPropertyNode;
 import com.aptana.editor.js.parsing.ast.JSParseRootNode;
+import com.aptana.index.core.Index;
+import com.aptana.index.core.IndexManager;
 import com.aptana.parsing.ast.IParseNode;
 
 public class JSHyperlinkDetector extends AbstractHyperlinkDetector
@@ -54,13 +71,72 @@ public class JSHyperlinkDetector extends AbstractHyperlinkDetector
 
 			if (ast instanceof JSParseRootNode)
 			{
-				// walk AST with custom TreeWalker
-				JSHyperlinkCollector collector = new JSHyperlinkCollector(region.getOffset());
+				int offset = region.getOffset();
 
+				// walk AST to grab potential hyperlinks
+				JSHyperlinkCollector collector = new JSHyperlinkCollector(offset);
 				((JSParseRootNode) ast).accept(collector);
-
-				// grab results
 				result = collector.getHyperlink();
+
+				if (result != null)
+				{
+					IParseNode node = ast.getNodeAtOffset(offset);
+					JSLocationIdentifier identifier = new JSLocationIdentifier(offset, node);
+					((JSParseRootNode) ast).accept(identifier);
+					LocationType type = identifier.getType();
+
+					switch (type)
+					{
+						case IN_PROPERTY_NAME:
+						{
+							JSGetPropertyNode getPropertyNode = ASTUtil.getGetPropertyNode(identifier.getTargetNode(),
+									identifier.getStatementNode());
+							Index index = getIndex(editor);
+							URI location = getURI(editor);
+							List<String> types = ASTUtil.getParentObjectTypes(index, location, node, getPropertyNode,
+									offset);
+
+							if (types != null && !types.isEmpty())
+							{
+								String typeName = types.get(0);
+								JSIndexQueryHelper queryHelper = new JSIndexQueryHelper();
+								TypeElement typeElement = queryHelper.getType(index, typeName, true);
+
+								System.out.println(typeElement.toSource());
+
+								@SuppressWarnings("unchecked")
+								List<String> documents = typeElement.getDocuments();
+								if (documents != null && !documents.isEmpty())
+								{
+									((JSHyperlink) result).setFilePath(documents.get(0));
+									System.out.println(documents);
+								}
+							}
+							break;
+						}
+
+						case IN_VARIABLE_NAME:
+						{
+							String name = node.getText();
+							Index index = getIndex(editor);
+							JSIndexQueryHelper queryHelper = new JSIndexQueryHelper();
+							PropertyElement global = queryHelper.getGlobal(index, name);
+
+							System.out.println(global.toSource());
+
+							List<String> documents = global.getDocuments();
+							if (documents != null && !documents.isEmpty())
+							{
+								((JSHyperlink) result).setFilePath(documents.get(0));
+								System.out.println(documents);
+							}
+							break;
+						}
+
+						default:
+							break;
+					}
+				}
 			}
 		}
 
@@ -80,6 +156,64 @@ public class JSHyperlinkDetector extends AbstractHyperlinkDetector
 		if (textViewer instanceof IAdaptable)
 		{
 			result = (AbstractThemeableEditor) ((IAdaptable) textViewer).getAdapter(AbstractThemeableEditor.class);
+		}
+
+		return result;
+	}
+
+	/**
+	 * getIndex
+	 * 
+	 * @param editor
+	 * @return
+	 */
+	protected Index getIndex(AbstractThemeableEditor editor)
+	{
+		Index result = null;
+
+		if (editor != null)
+		{
+			IEditorInput editorInput = editor.getEditorInput();
+
+			if (editorInput instanceof IFileEditorInput)
+			{
+				IFileEditorInput fileEditorInput = (IFileEditorInput) editorInput;
+				IFile file = fileEditorInput.getFile();
+				IProject project = file.getProject();
+				result = IndexManager.getInstance().getIndex(project.getLocationURI());
+			}
+			else if (editorInput instanceof IURIEditorInput)
+			{
+				IURIEditorInput uriEditorInput = (IURIEditorInput) editorInput;
+				URI uri = uriEditorInput.getURI();
+				// FIXME This file may be a child, we need to check to see if there's an index with a parent URI.
+				result = IndexManager.getInstance().getIndex(uri);
+			}
+		}
+
+		return result;
+	}
+
+	/**
+	 * getURI
+	 * 
+	 * @param editor
+	 * @return
+	 */
+	protected URI getURI(AbstractThemeableEditor editor)
+	{
+		URI result = null;
+
+		if (editor != null)
+		{
+			IEditorInput editorInput = editor.getEditorInput();
+
+			if (editorInput instanceof IURIEditorInput)
+			{
+				IURIEditorInput fileEditorInput = (IURIEditorInput) editorInput;
+
+				result = fileEditorInput.getURI();
+			}
 		}
 
 		return result;
