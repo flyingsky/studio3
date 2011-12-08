2cd559266044b2d6bc7daae0285ca88bf344cab5
Kevin Lindsey <klindsey@appcelerator.com>
2011-12-08 10:57:02 -0600

    Basic unit tests

--- tests/com.aptana.editor.js.tests/src/com/aptana/editor/js/hyperlink/JSHyperlinkDetectorTests.java
+++ tests/com.aptana.editor.js.tests/src/com/aptana/editor/js/hyperlink/JSHyperlinkDetectorTests.java
@@ -10,6 +10,7 @@ package com.aptana.editor.js.hyperlink;
 import org.eclipse.jface.text.Region;
 import org.eclipse.jface.text.hyperlink.IHyperlink;
 
+import com.aptana.core.util.StringUtil;
 import com.aptana.editor.common.AbstractThemeableEditor;
 import com.aptana.editor.js.contentassist.index.JSFileIndexingParticipant;
 import com.aptana.editor.js.tests.JSEditorBasedTests;
@@ -26,28 +27,117 @@ public class JSHyperlinkDetectorTests extends JSEditorBasedTests
 		return new JSFileIndexingParticipant();
 	}
 
-	public void testGlobal()
+	public void assertHyperlinks(String resource, JSHyperlink... expectedHyperlinks)
 	{
-		String resource = "hyperlinks/global.js";
-
 		setupTestContext(resource);
 
 		if (editor instanceof AbstractThemeableEditor)
 		{
 			AbstractThemeableEditor themeableEditor = (AbstractThemeableEditor) editor;
+
+			// only supporting testing of one offset per file
+			assertTrue(cursorOffsets.size() > 0);
+			int offset = cursorOffsets.get(0);
+
+			// grab the links for our offset
 			JSHyperlinkDetector detector = new JSHyperlinkDetector();
+			IHyperlink[] hyperlinks = detector.detectHyperlinks(themeableEditor, new Region(offset, 0), true);
 
-			for (int offset : cursorOffsets)
+			// make sure we got as many links as we expected. Note that we expect a null result when no links are
+			// expected
+			if (expectedHyperlinks == null || expectedHyperlinks.length == 0)
 			{
-				IHyperlink[] hyperlinks = detector.detectHyperlinks(themeableEditor, new Region(offset, 0), true);
-
+				assertNull(hyperlinks);
+			}
+			else
+			{
 				assertNotNull(hyperlinks);
+				assertEquals(expectedHyperlinks.length, hyperlinks.length);
+			}
 
-				// check count
-				assertEquals(1, hyperlinks.length);
+			for (int i = 0; i < expectedHyperlinks.length; i++)
+			{
+				// check type
+				IHyperlink hyperlink = hyperlinks[i];
+				assertTrue(hyperlink instanceof JSHyperlink);
 
 				// check content
+				// assertEquals(targetHyperlinks[i], hyperlink);
+				assertHyperlink(expectedHyperlinks[i], (JSHyperlink) hyperlink);
 			}
 		}
+	}
+
+	protected void assertHyperlink(JSHyperlink a, JSHyperlink b)
+	{
+		// @formatter:off
+		assertEquals("Hyperlink regions do not match", a.getHyperlinkRegion(), b.getHyperlinkRegion());
+		assertTrue(
+			"Labels do not match. Expected '" + a.getTypeLabel() + "', but found '" + b.getTypeLabel() + "'",
+			StringUtil.areEqual(a.getTypeLabel(), b.getTypeLabel())
+		);
+		assertTrue("Text values do not match", b.getHyperlinkText().endsWith(a.getHyperlinkText()));
+		assertTrue("File paths do not match", b.getTargetFilePath().endsWith(a.getTargetFilePath()));
+		assertTrue(
+			"Search strings do not match. Expected '" + a.getSearchString() + "', but found '" + b.getSearchString() + "'",
+			StringUtil.areEqual(a.getSearchString(), b.getSearchString())
+		);
+		assertEquals("Target regions do not match", a.getTargetRegion(), b.getTargetRegion());
+		// @formatter:on
+	}
+
+	public void testGlobal()
+	{
+		String resource = "hyperlinks/global.js";
+
+		assertHyperlinks(resource, new JSHyperlink(new Region(28, 3), "variable", resource, resource, "abc"));
+	}
+
+	public void testPropertyIsFunctionDeclaration()
+	{
+		String resource = "hyperlinks/propertyIsFunctionDeclaration.js";
+
+		// NOTE: we should not get any links here
+		assertHyperlinks(resource);
+	}
+
+	public void testPropertyIsFunction()
+	{
+		String resource = "hyperlinks/propertyIsFunction.js";
+		JSHyperlink link = new JSHyperlink(new Region(34, 3), "", resource, resource, "def");
+
+		assertHyperlinks(resource, link);
+	}
+
+	public void testParameter()
+	{
+		String resource = "hyperlinks/parameter.js";
+		JSHyperlink link = new JSHyperlink(new Region(39, 3), "parameter", resource, resource, new Region(13, 3));
+
+		assertHyperlinks(resource, link);
+	}
+
+	public void testNestedParameter()
+	{
+		String resource = "hyperlinks/nestedParameter.js";
+		JSHyperlink link = new JSHyperlink(new Region(71, 3), "parameter", resource, resource, new Region(13, 3));
+
+		assertHyperlinks(resource, link);
+	}
+
+	public void testLocalDeclaration()
+	{
+		String resource = "hyperlinks/localDeclaration.js";
+		JSHyperlink link = new JSHyperlink(new Region(42, 3), "local", resource, resource, new Region(22, 3));
+
+		assertHyperlinks(resource, link);
+	}
+
+	public void testLocalAssignment()
+	{
+		String resource = "hyperlinks/localAssignment.js";
+		JSHyperlink link = new JSHyperlink(new Region(42, 3), "local", resource, resource, new Region(18, 3));
+
+		assertHyperlinks(resource, link);
 	}
 }
