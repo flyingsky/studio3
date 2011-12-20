debb26c8fa50c00237821ae61f8c25e321a5878a
Kevin Lindsey <klindsey@appcelerator.com>
2011-12-20 10:55:10 -0600

    Show an event icon on event elements in the index view

--- plugins/com.aptana.editor.js/src/com/aptana/editor/js/views/JSIndexViewLabelProvider.java
+++ plugins/com.aptana.editor.js/src/com/aptana/editor/js/views/JSIndexViewLabelProvider.java
@@ -20,6 +20,7 @@ import com.aptana.editor.js.contentassist.JSModelFormatter;
 import com.aptana.editor.js.contentassist.model.BaseElement;
 import com.aptana.editor.js.contentassist.model.ClassElement;
 import com.aptana.editor.js.contentassist.model.ClassGroupElement;
+import com.aptana.editor.js.contentassist.model.EventElement;
 import com.aptana.editor.js.contentassist.model.FunctionElement;
 import com.aptana.editor.js.contentassist.model.JSElement;
 import com.aptana.editor.js.contentassist.model.PropertyElement;
@@ -35,6 +36,7 @@ public class JSIndexViewLabelProvider implements ILabelProvider, ILabelDecorator
 	private static final Image PROPERTY_ICON = JSPlugin.getImage("icons/js_property.png"); //$NON-NLS-1$
 	private static final Image FOLDER_ICON = JSPlugin.getImage("icons/folder.png"); //$NON-NLS-1$
 	private static final Image SCRIPT_ICON = JSPlugin.getImage("icons/js.png"); //$NON-NLS-1$
+	private static final Image EVENT_ICON = JSPlugin.getImage("icons/event.gif"); //$NON-NLS-1$
 
 	private static final ImageDescriptor STATIC_OVERLAY = JSPlugin.getImageDescriptor("icons/overlays/static.png");
 
@@ -123,8 +125,12 @@ public class JSIndexViewLabelProvider implements ILabelProvider, ILabelDecorator
 		}
 		else if (element instanceof PropertyElement)
 		{
-			// Use same logic we use in CA to determine icon type
+			// Use same logic we use in CA to determine icon type. This gives us individual icons by property type
 			result = JSModelFormatter.getImage((PropertyElement) element);
+		}
+		else if (element instanceof EventElement)
+		{
+			result = EVENT_ICON;
 		}
 		else
 		{
